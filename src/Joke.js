7c5c8cbc543a15c5b25e657cd297e1e75b4be4d4
RoigRosa201 <roigrosa88@gmail.com>
2019-07-19 19:15:51 -0400

    fixed json and created arrays to fetch the jokes resultsused component used effect hooks to fecth it from the api with JSX

--- src/Joke.js
+++ src/Joke.js
@@ -1,17 +1,29 @@
 import React, { useEffect, useState } from 'react';
 
 function Joke() {
+    const [joke, setJoke] = useState({});
+    
     useEffect(() => {
         fetch('https://official-joke-api.appspot.com/jokes/random')
+       // fetch('http://localhost:3001/Jokes/random')
             .then(response => response.json())
-            .then(json => console.log('Joke json', json));
-    });
-
+            .then(json => {
+                console.log('joke json', json);
+                setJoke(json);
+        });
+    },  []);
+    
+    const { setup, punchline } = joke;
     return (
         <div> 
-            <h3>Joke</h3>
+            <h3>Joke of the session</h3>
+            <p>{setup}</p>
+            <p><em>
+             {punchline}
+            </em></p>
         </div>
-    )
-}
+        )
+     }
+    
 
-export default Joke;
+    export default Joke;
